e126d319187b3f60fc228db4667cef1d6913c667
63070055 <73680542+63070055@users.noreply.github.com>
2020-10-30 12:32:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php
-echo "<h2>Nattapong Waijaree</h2>";
-echo "nickname Earth<br>";
-?>
+<html>
+    <head>
+        <title>Hello world</title>
+        <meiecharset="UTF-8>
+    </head>
+    <body>
+        <h1>My Nattapong Waijaree</h1>
+        <p>My My nickname Earth</p>
+        <img src="e1_3.jpg">
+        <img src="e7630a608e5b1b9a558948608fc5d710.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <u1>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </u1>
+
+    </body>
+</html>   
