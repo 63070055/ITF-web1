205ec86eb461faef1e806f79fdc046e005600a46
63070055 <73680542+63070055@users.noreply.github.com>
2020-11-13 12:35:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LAB-ITF 63070055</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<title>ITF Lab</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <img src="e1_3.jpg" style="height:300px;" style="width: 100px;"class="img-fluid rounded" />
-            </div>
-            <div class="col-md-3">
-                <img src="download (1).jpg" style="height:100px;" style="width: 50px;"class="img-fluid img-thumbnail" />
-                <img src="download.jpg" style="height:100px;" style="width: 50px;"class="img-fluid img-thumbnail" />
-                <img src="_ขายแมวเมนคูน1-e1425953981501-750x410.jpg" style="height:100px;" style="width: 50px;"class="img-fluid img-thumbnail" />
-            </div>
-            <div class="col-md-3">
-                <img src="Maine-Coon-02.jpg" style="height: 200px;" style="width: 50px;" class="img-fluid" />
-                <img src="thailand-krabi-rok-island-gettyimages-508411800-news.jpg" style="height: 100px;" style="width: 50px;" class="img-fluid img-thumbnail" />
-            </div>
-        </div> <!-- Close Row -->
-    </div> <!-- Close container -->
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
 </body>
-</html>  
+</html>
