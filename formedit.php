0b5acdf91d6141bc3b38ee5974faa40f8fb27700
63070055 <73680542+63070055@users.noreply.github.com>
2020-11-19 15:44:58 +0700

    Update formedit.php

--- formedit.php
+++ formedit.php
@@ -8,7 +8,7 @@
   <form action = "edit2.php" method = "post" id="CommentForm" >
     Name to edit:<br>
     <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
-    ความรู้สึกที่เปลี่ยนไป:<br>
+    Comment to edit:<br>
     <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter your feel"></textarea><br>  
     <input type="submit" id="commentBtn">
   </form> 
